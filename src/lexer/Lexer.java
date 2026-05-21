2e4ea65129fcc7d064f9acb9b264e26e1ed93a3d
lelehyeok <limorenoha@ittepic.edu.mx>
2026-05-21 15:07:51 -0700

    Primer Commit

---
+++ src/lexer/Lexer.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lexer;
+
+/**
+ *
+ * @author akmh1
+ */
+public class Lexer {
+  private final String input;
+    private int position;
+    private char currentChar;
+    private TokenType lastTokenType;
+
+    // El constructor recibe la fórmula (ej: "2Ca(OH)2") y prepara el primer caracter
+    public Lexer(String input) {
+        this.input = input;
+        this.position = 0;
+        this.currentChar = input.length() > 0 ? input.charAt(0) : '\0';
+    }
+    
+    
+
+    // Función para avanzar un paso en nuestra máquina de estados
+    private void advance() {
+        position++;
+        if (position >= input.length()) {
+            currentChar = '\0'; // \0 significa que llegamos al final del texto
+        } else {
+            currentChar = input.charAt(position);
+        }
+    }
+    private Token createToken(TokenType type, String lexeme) {
+        this.lastTokenType = type;
+        return new Token(type, lexeme);
+    }
+
+    // El motor principal 
+    public Token getNextToken() {
+        while (currentChar != '\0') {
+            
+            // 1. FILTRADO: Ignorar espacios en bco o saltos de línea
+            if (Character.isWhitespace(currentChar)) {
+                advance();
+                continue;
+            }
+
+            // 2. Automata para flecha y signos simples
+            if (currentChar == '-') {
+                if (position + 1 < input.length() && input.charAt(position + 1) == '>') {
+                    advance(); 
+                    advance();
+                    return createToken(TokenType.ARROW, "->");
+                }
+            }
+            if (currentChar == '(') {
+                advance();
+                return createToken(TokenType.LPAREN, "(");
+            }
+            if (currentChar == ')') {
+                advance();
+                return createToken(TokenType.RPAREN, ")");
+            }
+            if (currentChar == '+') {
+                advance();
+                return createToken(TokenType.PLUS, "+");
+            }
+            if (currentChar == '=') {
+                advance();
+                return createToken(TokenType.ASSIGN, "=");
+            }
+
+            // Automata elementos, numeros, palabras reservadas
+            
+            // 3. MANEJO DE ERRORES: Si lee un símbolo inválido (ej: @, %)
+            throw new RuntimeException("Error léxico: Caracter no reconocido '" + currentChar + "' en la posición " + position);
+        }
+            // 4. Automata para coeficientes y subindices
+                if (Character.isDigit(currentChar)) {
+                StringBuilder sb = new StringBuilder();
+                while (currentChar != '\0' && Character.isDigit(currentChar)) {
+                    sb.append(currentChar);
+                    advance();
+                }
+                // Analiza el contexto previo
+                if (lastTokenType == TokenType.ELEMENT || lastTokenType == TokenType.RPAREN) {
+                    return createToken(TokenType.SUBSCRIPT, sb.toString());
+                } else {
+                    return createToken(TokenType.COEFFICIENT, sb.toString());
+                }
+            }
+
+            // 5. Automata de identificadores (Ej: _mezcla, _M1)
+                if (currentChar == '_') {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(currentChar);
+                    advance(); // Consumimos el guion bajo inicial
+    
+            // El siguiente caracter DEBE ser una letra
+                if (currentChar != '\0' && Character.isLetter(currentChar)) {
+                    sb.append(currentChar);
+                    advance();
+            // A partir de aquí (tercer caracter), ya podemos aceptar letras o números
+                while (currentChar != '\0' && Character.isLetterOrDigit(currentChar)) {
+                    sb.append(currentChar);
+                    advance();
+                }
+        
+                return createToken(TokenType.IDENTIFIER, sb.toString());
+                } else {
+            // Si después del '_' hay un número, un espacio o nada, es un error léxico
+                throw new RuntimeException("Error léxico: Un identificador debe llevar una letra después del guion bajo. Encontrado: '" + currentChar + "' en la posición " + position);
+                }
+                }
+        // fin del archivo o fila
+        return new Token(TokenType.EOF, "");
+    }  
+}
