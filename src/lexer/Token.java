e25564b0403b5aba9efa53c648701b364eb7cb88
lelehyeok <limorenoha@ittepic.edu.mx>
2026-05-22 10:12:35 -0700

    Parser a la mitad

---
+++ src/lexer/Token.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lexer;
+
+/**
+ *
+ * @author akmh1
+ */
+public class Token {
+    private final TokenType type;
+    private final String lexeme;
+
+    public Token(TokenType type, String lexeme) {
+        this.type = type;
+        this.lexeme = lexeme;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" + "type=" + type + ", lexeme='" + lexeme + "'}";
+    }
+}
